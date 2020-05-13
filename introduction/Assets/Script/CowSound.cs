21138550661c7e34ce051d915f1d7c192731b4e8
Moshe Elhadad <44942115+moshelh@users.noreply.github.com>
2020-05-13 15:35:38 +0300

    new commit

---
+++ introduction/Assets/Script/CowSound.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CowSound : MonoBehaviour
+{
+    [SerializeField]
+    AudioSource source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("PlaySound", UnityEngine.Random.Range(5, 200)*Time.deltaTime);
+    }
+    
+    void PlaySound()
+    {
+        if (!source.isPlaying)
+        {
+            source.Play();
+        }
+        Invoke("PlaySound", UnityEngine.Random.Range(500, 1000)* Time.deltaTime);
+    }
+
+}
---
+++ introduction/Assets/Script/Explosion_Destroy.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion_Destroy : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        Destroy(gameObject, 3.0f);
+    }
+}
